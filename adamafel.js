418690d664a18fd6459243099a6d49cf10de822c
mhhl <fake@hotmail.com>
2020-02-28 17:53:54 -0600

    todo de adamafel

---
+++ adamafel.js
@@ -0,0 +1,54 @@
+$(".navbar-brand").on("click", function(){
+    $(".link").removeClass("linkSelect")
+});
+
+
+$(".link").on("click", function(){
+    $(".link").removeClass("linkSelect")
+    $(this).addClass("linkSelect");
+});
+
+$(".link").on("mouseenter", function(){
+    $(this).addClass("linkHover")
+});
+$(".link").on("mouseleave", function(){
+    $(this).removeClass("linkHover")
+});
+
+
+
+
+setInterval(cambiarFoto, 4000);
+var x = 1;
+function cambiarFoto(){
+    x++;
+    if (x > 7){
+        x=1;
+    }
+    
+    $(".imgPortada").attr("src", "images/foto"+x+".jpg");
+    
+}
+
+
+// Funcionalidad de botones principales
+$("#butPrincipal").on("click", function(){
+    $("section").addClass("d-none")
+    $("#p1").removeClass("d-none");  
+});
+$("#butTecnologia").on("click", function(){
+    $("section").addClass("d-none")
+    $("#pagTecnologia").removeClass("d-none");
+});
+$("#butProyectos").on("click", function(){
+    $("section").addClass("d-none")
+    $("#pagProyectos").removeClass("d-none");
+});
+$("#butNosotros").on("click", function(){
+    $("section").addClass("d-none")
+    $("#pagNosotros").removeClass("d-none");
+});
+$("#butContacto").on("click", function(){
+    $("section").addClass("d-none")
+    $("#pagContacto").removeClass("d-none");
+});
